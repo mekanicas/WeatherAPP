e8ba003bd3f5d45565774f23d30096f9f6acdc68
Mekanicas <brunomicelio52@gmail.com>
2024-05-03 15:43:59 -0300

    adding api and style

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const apiKey = "f6783d918203defd90a49836c3aabc09";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    let data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind").innerHTML = data.wind.speed;
+
+}
+
+checkWeather();
